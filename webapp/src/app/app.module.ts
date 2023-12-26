0d66bd849e8086a1b45e311e4742317b0aa54d9e
Connor McDonald <connormcd98@gmail.com>
2023-12-26 13:22:23 +0200

    generate login boilerplate

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TechComponent } from './tech/tech.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TechComponent
+    TechComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
