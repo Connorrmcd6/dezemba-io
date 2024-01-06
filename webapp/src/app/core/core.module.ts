5dbeb484412b625ec7d7b72cc0ae6ae27cae7a4c
Connor McDonald <connormcd98@gmail.com>
2024-01-06 14:01:25 +0200

    responsive margins

--- webapp/src/app/core/core.module.ts
+++ webapp/src/app/core/core.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 
@@ -9,7 +14,11 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ],
   exports: [
     HeaderComponent
