9c0801a9721dcabab99c70f882ef6dc0fd44e43e
Connor McDonald <connormcd98@gmail.com>
2024-01-08 17:51:46 +0200

    routing done

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+//paths
 import { LoginComponent } from './modules/auth/login/login.component';
 import { LandingComponent } from './core/landing/landing.component';
 
--- webapp/src/app/core/core.module.ts
+++ webapp/src/app/core/core.module.ts
@@ -9,6 +9,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 // Components
 import { FooterComponent } from './footer/footer.component';
@@ -27,7 +28,8 @@ import { LandingComponent } from './landing/landing.component';
     MatListModule,
     MatMenuModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule
   ],
   exports: [
     LandingComponent,
