6c837aaeb1f67721bb5b0c6e5c314865a21046f6
Connor McDonald <connormcd98@gmail.com>
2024-01-14 08:11:00 +0200

    clean up

--- webapp/src/app/modules/auth/auth.module.ts
+++ webapp/src/app/modules/auth/auth.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,13 +10,13 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent,
-    
+    RegisterComponent
   ],
   imports: [
     CommonModule,
